489a2e9582ea75031cb373180d3de0c9214ddffc
daniel antohe <dantohe@gmail.com>
2017-06-09 06:35:43 -0600

    script changes

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-display("Hello World")
+"use sctrict";
+var cat = {
+    name: "mushy",
+    color: "black",
+    grow: function() {
+        display("growwwwwww")
+    }
+};
+cat.age = 5;
+cat.speak = function() {
+    display("meeeeeeeeee")
+};
+
+
+function Cat(name, color) {
+    this.name = name
+    this.color = color
+}
+
+var caterina = new Cat("porky","white");
+
+display(caterina.name)
+// cat.speak();
+// cat.grow();
+
+
+
+// display(cat.name+" "+cat.age)
