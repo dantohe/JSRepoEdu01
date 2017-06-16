af5a79dd4e7d3ed67c6af2dcfcdfd9dff08235d8
daniel antohe <dantohe@gmail.com>
2017-06-16 11:59:10 -0600

    module design pattern implementation

---
+++ task.js
@@ -0,0 +1,19 @@
+var Repo = require('./taskRepository');
+
+var Task = function(data) {
+    this.name = data.name;
+    this.completed = false;
+    console.log('initiated task with name: '+data.name);
+}
+
+Task.prototype.complete = function() {
+    console.log('completing task ...');
+    this.completed = true;
+};
+
+Task.prototype.save = function() {
+    console.log('Saving task ...');
+    Repo.save(this);
+};
+
+module.exports =Task;
